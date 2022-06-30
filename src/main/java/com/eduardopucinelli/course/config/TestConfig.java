c299f4b96482d44e554984dc1b80963114824121
dudupuci <dudupucinelli@gmail.com>
2022-06-30 12:41:59 -0300

    Payment OneToOne association

--- src/main/java/com/eduardopucinelli/course/config/TestConfig.java
+++ src/main/java/com/eduardopucinelli/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.eduardopucinelli.course.entities.Category;
 import com.eduardopucinelli.course.entities.Order;
 import com.eduardopucinelli.course.entities.OrderItem;
+import com.eduardopucinelli.course.entities.Payment;
 import com.eduardopucinelli.course.entities.Product;
 import com.eduardopucinelli.course.entities.User;
 import com.eduardopucinelli.course.enums.OrderStatus;
@@ -35,7 +36,7 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private ProductRepository productRepository;
-	
+
 	@Autowired
 	private OrderItemRepository orderItemRepository;
 
@@ -79,10 +80,12 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
 		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
-		
+
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
-		
-		
+
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-21T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 
 }
