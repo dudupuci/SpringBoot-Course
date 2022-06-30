61371d46d073c7dd3ee4fcc96e9cbc7a2bcce78b
dudupuci <dudupucinelli@gmail.com>
2022-06-30 15:51:03 -0300

    User update

--- src/main/java/com/eduardopucinelli/course/services/UserService.java
+++ src/main/java/com/eduardopucinelli/course/services/UserService.java
@@ -23,10 +23,25 @@ public class UserService {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.get();
 	}
-	
+
 	public User insert(User obj) {
 		return userRepository.save(obj);
 	}
 
-	
+	public void delete(Long id) {
+		userRepository.deleteById(id);
+	}
+
+	public User update(Long id, User obj) {
+		User entity = userRepository.getOne(id);
+		updateData(entity, obj);
+		return userRepository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+
+	}
 }
